46e890a10c7b350809f2c84d40fcd531e41d4a45
AK-dev5 <aidkijamet22@gmail.com>
2025-07-15 12:58:44 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("active");
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  document.querySelectorAll(".animate").forEach(el => {
+    observer.observe(el);
+  });
+});
