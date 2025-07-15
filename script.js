36945a40f5d46dc9ae2575ca40317a4fc53697fd
AK-dev5 <aidkijamet22@gmail.com>
2025-07-15 18:05:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Scroll-Animationen für Sektionen
 window.addEventListener('scroll', () => {
-  document.querySelectorAll('.info, .kontakt, .hero').forEach(section => {
+  document.querySelectorAll('section').forEach(section => {
     const sectionPos = section.getBoundingClientRect().top;
     const screenPos = window.innerHeight / 1.3;
 
@@ -10,9 +10,8 @@ window.addEventListener('scroll', () => {
   });
 });
 
-// Klickanimation für Kontaktinfo
-const kontaktLinks = document.querySelectorAll('a');
-kontaktLinks.forEach(link => {
+// Klickanimation für Kontaktlinks
+document.querySelectorAll('a').forEach(link => {
   link.addEventListener('click', () => {
     link.style.color = '#00cc99';
     link.style.transition = 'color 0.3s ease';
